842c158b8536e3af1e1d618473a3e00eb0ea1943
Rafael Dias <faaell@gmail.com>
2023-09-30 16:46:56 -0300

    add post and get

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+const users = [
+    {id: 1, name: "Daniel", age:40},
+    {id: 2, name: "Gabriel", age:30},
+    {id: 3, name: "Magno", age:20},
+    {id: 4, name: "Wagner", age:10}
+];
+
+app.use(bodyParser.json());
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name, 
+        age: req.body.age
+    };
+
+    users.push(newUser);
+    res.json(newUser);
+    // res.send('Got a POST request')
+})
+
+app.put('/users', (req, res) => {
+    res.send('Got a PUT request')
+})
+
+app.delete('/users', (req, res) => {
+    res.send('Got a DELETE request')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on http://localhost:${port}`)
 })
